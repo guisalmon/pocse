90e3106911fd180771e05ec907cd6cb0e536837e
Rob <guisalmon@gmail.com>
2019-11-22 18:41:30 +0100

    Allow edition of 6 main colors

--- pop_customization.sh
+++ pop_customization.sh
@@ -38,9 +38,9 @@ getClosestColor () {
 		offsetG=$((ig-g))
 		offsetB=$((ib-b))
 
-		if [ $offsetR -lt 0 ] ; then offsetR=$((0-offsetR)) ; fi
-		if [ $offsetG -lt 0 ] ; then offsetG=$((0-offsetG)) ; fi
-		if [ $offsetB -lt 0 ] ; then offsetB=$((0-offsetB)) ; fi
+		[ $offsetR -lt 0 ] && offsetR=$((0-offsetR))
+		[ $offsetG -lt 0 ] && offsetG=$((0-offsetG))
+		[ $offsetB -lt 0 ] && offsetB=$((0-offsetB))
 
 		offset=$((offsetR+offsetG+offsetB))
 
@@ -60,7 +60,7 @@ colorCodeCached () {
 	code="-1"
 	while read l ; do
 		line=($l)
-		if [ $1 == ${line[0]} ] ; then code=${line[1]} ; fi
+		[ $1 == ${line[0]} ] && code=${line[1]}
 	done < $colorCodes
 	echo $code
 }
@@ -101,7 +101,8 @@ man='\n -d \t Force download of reference theme from Pop OS git repo.
 \n -h \t Display this help and exit.
 \n -c \t Use terminal colors for preview (compatible with most modern terminals)
 \n -e \t Edit theme after parsing of colors
-\n -u [DIRECTORY] \t Update a previously edited theme.'
+\n -u [DIRECTORY] \t Update a previously edited theme
+\n -o NAME \t Specify a name for the new theme'
 
 d=false
 i=false
@@ -109,6 +110,7 @@ r=false
 c=false
 e=false
 u=false
+o=false
 lastArg=""
 for arg in "$@"
 do
@@ -138,9 +140,12 @@ do
 	"-u")
 		u=true
 		;;
+	"-o")
+		o=true
+		;;
 	*)
-		if [[ $lastArg == -u ]]
-		then
+		case $lastArg in 
+		"-u")
 			if [[ $arg != "" && -d $arg && -f $arg$shellColors && -f $arg$gtkShellColors && -f $arg$popOsColors && -f $arg$gtkPopOsColors ]]
 			then
 				refDir=$arg
@@ -148,14 +153,25 @@ do
 				echo "$arg is not a valid theme directory"
 				exit
 			fi
-		else
+			;;
+		"-o")
+			if [[ $arg != "" ]]
+			then
+				modDir=$modPrefix$userPrefix$arg
+				exportDir=$userPrefix$arg
+			else exit
+			fi
+			;;
+		*)
 			echo "Unknown argument $arg"
 			exit
-		fi
 		;;
+		esac
 	esac
 	lastArg=$arg
 done
+
+$o && [[ $modDir == "" ]] && exit
 
 if $u
 then
@@ -180,7 +196,7 @@ else
 	refDir="Pop_Reference"
 fi
 
-if [ ! -d $resDir ] ; then mkdir $resDir ; fi
+[ ! -d $resDir ] && mkdir $resDir
 
 if ! $d
 then
@@ -566,12 +582,12 @@ displayGTK () {
 }
 
 displayGTK | column -t
-echo -e "\n"
 
 # Done displaying parsed colors
 
 if ! $e
 then
+	echo -e "\n"
 	echo -e "Do you want to edit the theme? [Y/n]"
 	read input
 	case $input in
@@ -584,18 +600,174 @@ then
 	esac
 fi
 
-echo -e "How will you name your theme (cannot be empty)? \nIf you already defined a variant with this name, it will be overwritten." 
+if ! $o
+then
+	echo -e "How will you name your theme (cannot be empty)? \nIf you already defined a variant with this name, it will be overwritten." 
+	input=""
+	while [[ $input == "" ]]
+	do
+		echo -ne "$userPrefix"
+		read input
+	done
+	modDir=$modPrefix$userPrefix$input
+	exportDir=$userPrefix$input
+fi
+[ -d $modDir ] && rm -r $modDir
+cp -r $refDir $modDir
+echo -e "\nReference theme directory copied to $modDir where modifications will happen.\n"
+
+# Edit parsed colors
+
 input=""
-while [[ $input == "" ]]
+while [[ $input != "l" && $input != "d" ]]
 do
-	echo -ne "\n$userPrefix"
+	echo -ne "Edit [l]ight or [d]ark variant (later on you'll have the opportunity to mess up the other variant too): "
 	read input
 done
-modDir=$modPrefix$userPrefix$input
-exportDir=$userPrefix$input
-if [ -d $modDir ] ; then rm -r $modDir ; fi
-cp -r $refDir $modDir
-echo -e "Reference theme directory copied to $modDir where modifications will happen.\n"
 
+[[ $input == "l" ]] && variant=0 || variant=1
 
+input=""
+while [[ $input != "m" && $input != "a" ]]
+do
+	echo -ne "Edit only [m]ain colors or [a]ll color: "
+	read input
+done
 
+[[ $input == 'm' ]] && quick=true || quick=false
+
+if ! $quick
+then
+	echo "Edition of all colors not implemented yet, fallback to quick edition"
+	quick=true
+fi
+
+newColorArray=("\$orange" "\$highlights_orange" "\$text_orange" "\$blue" "\$highlights_blue" "\$text_blue")
+declare -a editedColorArray
+editedColorIndex=0
+declare -A gnomeNewColorMap
+
+echo -e "\nYou will be prompted with colors to edit, type in valid hexadecimal color codes (eg. #000000) to edit them or [ENTER] to keep them\n"
+
+gnomeGetUserInput () {
+	input="."
+	while ! [[ ${#input} -eq 7 && ${input:1} =~ ^[0-9A-Fa-f]{6}$ || $input == "" ]]
+	do
+		echo -ne "$1: `formatColor ${gnomeColorMap[$1,$variant]}` : "
+		read input
+	done
+	if [[ $input != "" ]]
+	then
+		gnomeNewColorMap[$1,$variant]=$input
+		editedColorArray[$editedColorIndex]=$1
+		(( editedColorIndex++ ))
+	fi
+}
+
+for color in ${newColorArray[*]}
+do
+	gnomeGetUserInput $color
+done
+
+[[ $variant == 0 ]] && variantName="light" || variantName="dark"
+[[ $variant == 0 ]] && newVariantName="dark" || newVariantName="light"
+
+input="."
+while [[ $input != "y" && $input != "Y" && $input != "n" && $input != "N" && $input != "yes" && $input != "Yes" && $input != "no" && $input != "No" && $input != "" ]]
+do
+	echo -ne "Do you want to edit $newVariantName theme? [Y/n]"
+	read input
+done
+
+case $input in
+	"y"|"yes"|""|"Yes"|"Y")
+		edit=true
+		;;
+	*)
+		edit=false
+		;;
+esac
+
+if $edit
+then
+	oldVariant=$variant
+	[[ $variant  == 0 ]] && variant=1 || variant=0
+
+	input="."
+	while [[ $input != "y" && $input != "Y" && $input != "n" && $input != "N" && $input != "yes" && $input != "Yes" && $input != "no" && $input != "No" && $input != "" ]]
+	do
+		echo -ne "Do you want to reuse colors from $variantName theme in $newVariantName theme where they were reused in reference theme? [Y/n]"
+		read input
+	done
+	case $input in
+		"y"|"yes"|""|"Yes"|"Y")
+			reuse=true
+			;;
+		*)
+			reuse=false
+			;;
+	esac
+
+	if $reuse
+	then
+		for color in ${editedColorArray[*]}
+		do
+			for otherColor in ${newColorArray[*]}
+			do
+				if [[ ${gnomeColorMap[$color,$oldVariant]} == ${gnomeColorMap[$otherColor,$variant]} ]]
+				then
+					if [[ ${#gnomeNewColorMap[$otherColor,0]} -ne 7 
+						&& ! ${gnomeNewColorMap[$otherColor,0]:1} =~ ^[0-9A-Fa-f]{6}$ 
+						&& ${#gnomeNewColorMap[$otherColor,1]} -ne 7 
+						&& ! ${gnomeNewColorMap[$otherColor,1]:1} =~ ^[0-9A-Fa-f]{6}$ 
+						&& ${#gnomeNewColorMap[$otherColor,2]} -ne 7 
+						&& ! ${gnomeNewColorMap[$otherColor,2]:1} =~ ^[0-9A-Fa-f]{6}$ ]]
+					then
+						editedColorArray[$editedColorIndex]=$otherColor
+						(( editedColorIndex++ ))
+					fi
+					blbl=${gnomeNewColorMap[$color,$oldVariant]}
+					gnomeNewColorMap[$otherColor,$variant]=$blbl
+				fi
+			done
+		done
+	fi
+
+	for color in ${editedColorArray[*]}
+	do 
+		if [[ ${#gnomeNewColorMap[$color,$variant]} -ne 7 
+			&& ! ${gnomeNewColorMap[$color,$variant]:1} =~ ^[0-9A-Fa-f]{6}$ ]]
+		then
+			gnomeGetUserInput $color
+		fi
+	done
+fi
+
+editionFormatColors () {
+	echo "$1 `formatColor $2` `formatColor $3` `formatColor $4` `formatColor $5` `formatColor $6` `formatColor $7`"
+}
+
+displayEdition () {
+	echo -e 'Edited Light New Dark New Neutral Neutral'
+	declare -a validColors	
+	size=${#gnomeNewColorMap[@]}
+	count=0
+	for color in ${editedColorArray[*]} 
+	do
+		for i in {0..2}
+		do
+			if [[ ${#gnomeNewColorMap[$color,$i]} -eq 7 && ${gnomeNewColorMap[$color,$i]:1} =~ ^[0-9A-Fa-f]{6}$ ]]
+			then 
+				validColors[$i]=${gnomeNewColorMap[$color,$i]}
+				(( count++ ))
+			else 
+				validColors[$i]=$placeholder
+			fi
+		done
+		echo -ne "Parsing color code $count of $size"\\r 1>&2
+		echo -e `editionFormatColors "$color" "${gnomeColorMap[$color,0]}" "${validColors[0]}" "${gnomeColorMap[$color,1]}" "${validColors[1]}" "${gnomeColorMap[$color,2]}" "${validColors[2]}"`
+	done 
+}
+
+echo -e "\n"
+displayEdition | column -t
